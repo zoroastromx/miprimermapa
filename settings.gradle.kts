8210703a2d20588fba2cb1c901ea4d0b18daef95
Alfredo10 <alfredo.marin@inegi.org.mx>
2024-05-21 13:53:30 -0600

    // para apuntar al repositorio de bonuspack

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        // para apuntar al repositorio de bonuspack
+        maven { url =  uri("https://jitpack.io") }
+    }
+}
+
+rootProject.name = "IRISapp"
+include(":app")
