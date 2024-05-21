9aef404bda9965ea532b9f5d1eb55c6af0dbed16
Alfredo10 <alfredo.marin@inegi.org.mx>
2024-05-21 14:53:25 -0600

    configuración de jitpack para osmdroid , osmbonuspack

---
+++ app/build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "inegi.org.mx.irisapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "inegi.org.mx.irisapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+//repositories { mavenCentral()
+  //             maven { url = uri("https://jitpack.io") } }
+
+//repositories {
+ //   mavenCentral()
+ //   flatDir {
+       // dirs 'libs'
+   // }
+//}
+
+dependencies {
+
+   // implementation(libs.androidx.legacy:legacy-support-v4)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.legacy.support.v4)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    //implementation("com.github.pedroSG94.RootEncoder:library:2.4.3") // prueba
+
+    //including using JitPack:
+
+    implementation("org.osmdroid:osmdroid-android:6.1.18")
+    implementation("com.github.MKergall:osmbonuspack:6.9.0")
+
+
+}
